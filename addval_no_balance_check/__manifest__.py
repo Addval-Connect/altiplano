ee06e6d143eb7912fa9ecb97df7a5d574a8231d2
DHerrmannP <dherrmann@addval.cl>
2022-06-14 15:36:05 -0400

    changed model file name, added view to manifest

--- addval_no_balance_check/__manifest__.py
+++ addval_no_balance_check/__manifest__.py
@@ -1,29 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "addval_no_balance_check",
-
-    'summary': """
+    "name": "addval_no_balance_check",
+    "summary": """
         USE WITH CAUTION: This module disables balance credit-debit check.
         USAR CON PRECAUCION: Este módulo desactiva la comprobación de saldo crédito-débito.
         """,
-
-    'description': """
+    "description": """
         Disables balance credit-debit check. / Desactiva la comprobación de saldo crédito-débito.
     """,
-
-    'author': "Addval Connect",
-    'website': "http://www.addval.io",
-
+    "author": "Addval Connect",
+    "website": "http://www.addval.io",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Accounting',
-    'version': '0.1',
-
+    "category": "Accounting",
+    "version": "0.1",
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account'],
-
+    "depends": ["base", "account"],
     "data": [
-      # "views/account_move_views.xml",
-    ]
+        "views/account_move_views.xml",
+    ],
 }
--- addval_no_balance_check/models/account_move.py
+++ addval_no_balance_check/models/account_move.py
@@ -2,8 +2,9 @@
 
 from odoo import models
 
-class AccountMove(models.Model):
-  _inherit = "account.move"
 
-  def _check_balanced(self):
-    return True
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _check_balanced(self):
+        return True
