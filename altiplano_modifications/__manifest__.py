20d49a64c3b2cfcd20bd211e58a3b975508f7d1d
DHerrmannP <dherrmann@addval.cl>
2022-10-19 11:53:18 -0300

    altiplano custom modifications

---
+++ altiplano_modifications/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Altiplano Custom Modifications (Addval)",
+    "summary": "Compilation of all altiplano custom modifications (Addval)",
+    "description": """This module adds the following modifications:
+    - Replace <Receptor>/<Contacto> with 44 in DTE when the partner is 61703000-4""",
+    "author": "Addval Connect",
+    "website": "http://www.addval.cl",
+    "category": "Accounting",
+    "version": "0.2",
+    "license": "Other proprietary",
+    "depends": ["l10n_cl_edi"],
+    "data": [],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
