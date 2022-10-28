b8f6ed33d8eaab84a8c9b06e22a2af47a1572a5c
DHerrmannP <dherrmann@addval.cl>
2022-10-28 17:31:56 -0300

    add contact override

--- altiplano_modifications/__manifest__.py
+++ altiplano_modifications/__manifest__.py
@@ -3,15 +3,16 @@
     "name": "Altiplano Custom Modifications (Addval)",
     "summary": "Compilation of all altiplano custom modifications (Addval)",
     "description": """This module adds the following modifications:
-    - Replace <Receptor>/<Contacto> with 44 in DTE when the partner is 61703000-4""",
+    - Replace <Receptor>/<Contacto> with move.contact_override in DTE if set""",
     "author": "Addval Connect",
     "website": "http://www.addval.cl",
     "category": "Accounting",
     "version": "0.2",
     "license": "Other proprietary",
-    "depends": ["l10n_cl_edi", "l10n_cl_edi_stock"],
+    "depends": ["l10n_cl_edi", "l10n_cl_edi_stock", "account"],
     "data": [
         "template/dte_template.xml",
+        "views/account_move_views.xml",
     ],
     "installable": True,
     "application": True,
